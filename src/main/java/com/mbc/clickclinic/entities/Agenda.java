c711d3f2827a3ab535366adb72dba095289f6b01
Hicham Ait hsaine <hicham.aithsaine@edu.uiz.ac.ma>
2022-03-31 12:09:02 +0000

    modifier entity agenda

--- src/main/java/com/mbc/clickclinic/entities/Agenda.java
+++ src/main/java/com/mbc/clickclinic/entities/Agenda.java
@@ -8,10 +8,12 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.util.Date;
 
 @Entity
 @Data @AllArgsConstructor @NoArgsConstructor
 public class Agenda {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private Date date;
 }
