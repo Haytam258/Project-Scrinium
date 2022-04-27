b25928eb873ccdde7ab42147b9a2b9f83d3b7dcf
Haytam258 <HaytamAboukherraz22@gmail.com>
2022-04-27 19:03:23 +0000

    added config for template access and some html modification

---
+++ src/main/java/com/mbc/clickclinic/configs/TemplateResolverConfiguration.java
@@ -0,0 +1,62 @@
+package com.mbc.clickclinic.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+
+@Configuration
+public class TemplateResolverConfiguration {
+    @Bean
+    public SpringResourceTemplateResolver firstTemplateResolver() {
+        SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+        templateResolver.setPrefix("/templates/secretaire/");
+        templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode(TemplateMode.HTML);
+        templateResolver.setCharacterEncoding("UTF-8");
+        templateResolver.setOrder(0);
+        templateResolver.setCheckExistence(true);
+
+        return templateResolver;
+    }
+
+    @Bean
+    public ClassLoaderTemplateResolver secondTemplateResolver() {
+        ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+        templateResolver.setPrefix("templates/admin/");
+        templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode(TemplateMode.HTML);
+        templateResolver.setCharacterEncoding("UTF-8");
+        templateResolver.setOrder(1);
+        templateResolver.setCheckExistence(true);
+
+        return templateResolver;
+    }
+
+    @Bean
+    public ClassLoaderTemplateResolver thirdTemplateResolver() {
+        ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+        templateResolver.setPrefix("templates/medecin/");
+        templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode(TemplateMode.HTML);
+        templateResolver.setCharacterEncoding("UTF-8");
+        templateResolver.setOrder(2);
+        templateResolver.setCheckExistence(true);
+
+        return templateResolver;
+    }
+
+    @Bean
+    public ClassLoaderTemplateResolver fourthTemplateResolver() {
+        ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+        templateResolver.setPrefix("templates/patient/");
+        templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode(TemplateMode.HTML);
+        templateResolver.setCharacterEncoding("UTF-8");
+        templateResolver.setOrder(3);
+        templateResolver.setCheckExistence(true);
+
+        return templateResolver;
+    }
+}
