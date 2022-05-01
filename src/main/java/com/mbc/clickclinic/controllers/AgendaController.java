d568aa3268de7d4733885f59684f592fcd9a03cc
JannateAl <jannatealqarh@gmail.com>
2022-05-01 13:10:32 +0000

    HTML indexAgenda/createAgenda & liaison backend for createAgenda but still needs to insert medecin_id

---
+++ src/main/java/com/mbc/clickclinic/controllers/AgendaController.java
@@ -0,0 +1,36 @@
+package com.mbc.clickclinic.controllers;
+
+import com.mbc.clickclinic.entities.Agenda;
+import com.mbc.clickclinic.service.AgendaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class AgendaController {
+
+    @Autowired
+    private AgendaService agendaService;
+
+    @GetMapping("/agenda/index")
+    public String index(){
+
+        return "indexAgenda";
+    }
+
+    @GetMapping("/agenda/create")
+    public String create(Model model){
+        Agenda agenda =new Agenda();
+        model.addAttribute("agenda", agenda);
+        return "createAgenda";
+    }
+
+    @PostMapping("/agenda/save")
+    public String saveAgenda(@ModelAttribute("agenda")Agenda agenda){
+        agendaService.createAgenda(agenda);
+        return "redirect:/agenda/index";
+    }
+}
--- src/main/java/com/mbc/clickclinic/entities/Agenda.java
+++ src/main/java/com/mbc/clickclinic/entities/Agenda.java
@@ -4,11 +4,14 @@ import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.*;
 import org.hibernate.Hibernate;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
 import java.time.LocalDate;
 import java.util.Date;
 import java.util.Objects;
+import java.util.TimeZone;
+
 
 @Entity
 @Getter
@@ -19,14 +22,16 @@ import java.util.Objects;
 public class Agenda {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-    @JsonFormat
-            (shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy hh:mm:ss")
+    /*@JsonFormat
+            (shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy hh:mm:ss")*/
+    @DateTimeFormat(pattern = "dd/MM/yyyy")
     private LocalDate dateDebut;
     //ajout d'attribut dateFin => necessaire pour visualiser la fin du conges
+    @DateTimeFormat(pattern = "dd/MM/yyyy")
     private LocalDate dateFin;
     private String jour;
-    private int heureDebut;
-    private int heureFin;
+    private String heureDebut;
+    private String heureFin;
     private String description;
 
     //Statut : 1 = Vacances, Fournisseurs etc (Pas disponible), Statut : 2 = Travail (Disponible),
